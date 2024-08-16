63efd0a3e608a203c18be57f24df7b7783abf310
Mustafa <mustafaumar70@live.com>
2024-08-16 16:04:58 +0100

    chore: v1.0.7

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'tsup'
-
+import { defineConfig } from 'tsup';
+ 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-})
+    format: ['cjs', 'esm'],
+    entry: ['./src/index.ts'],
+    dts: true,
+    shims: true,
+    skipNodeModulesBundle: true,
+    clean: true,
+});
